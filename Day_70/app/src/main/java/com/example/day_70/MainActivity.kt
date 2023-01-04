752250395e2877d41429c3cd5e1967cf26c0a7fb
KenStarry <starrycodes@gmail.com>
2023-01-04 23:50:23 +0300

    Added calendar dialog and time picker dialog

--- Day_70/app/src/main/java/com/example/day_70/MainActivity.kt
+++ Day_70/app/src/main/java/com/example/day_70/MainActivity.kt
@@ -1,24 +1,106 @@
 package com.example.day_70
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.day_70.ui.theme.Day_70Theme
+import com.maxkeppeker.sheets.core.models.base.rememberSheetState
+import com.maxkeppeler.sheets.calendar.CalendarDialog
+import com.maxkeppeler.sheets.calendar.models.CalendarConfig
+import com.maxkeppeler.sheets.calendar.models.CalendarSelection
+import com.maxkeppeler.sheets.calendar.models.CalendarStyle
+import com.maxkeppeler.sheets.clock.ClockDialog
+import com.maxkeppeler.sheets.clock.models.ClockConfig
+import com.maxkeppeler.sheets.clock.models.ClockSelection
+import java.time.LocalDate
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Day_70Theme {
-
+                MainScreen()
             }
         }
     }
 }
+
+@RequiresApi(Build.VERSION_CODES.O)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+
+    val context = LocalContext.current
+
+    val calendarState = rememberSheetState()
+    val clockState = rememberSheetState()
+
+    CalendarDialog(
+        state = calendarState,
+        selection = CalendarSelection.Dates { date ->
+            Log.d("Date", "$date")
+        },
+        config = CalendarConfig(
+            monthSelection = true,
+            yearSelection = true,
+            style = CalendarStyle.MONTH,
+            disabledDates = listOf(LocalDate.now().plusDays(7))
+        )
+    )
+
+    ClockDialog(
+        state = clockState,
+        selection = ClockSelection.HoursMinutes {hours, minutes ->
+            Toast.makeText(context, "$hours : $minutes", Toast.LENGTH_SHORT).show()
+        },
+        config = ClockConfig(
+            is24HourFormat = false
+        )
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Button(onClick = { calendarState.show() }) {
+            Text(text = "Date Picker")
+        }
+
+        Button(onClick = { clockState.show() }) {
+            Text(text = "Time Picker")
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
